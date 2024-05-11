44f4ed6962fcfed27363146c776165e165b5b3fa
israt6898 <israt6898@gmail.com>
2024-05-11 18:25:40 +0600

    finish footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
+import Footer from "../Components/Footer/Footer";
 
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
             <Navbar></Navbar>
             <Outlet></Outlet>
             <ToastContainer />
+            <Footer></Footer>
             
         </div>
     );
